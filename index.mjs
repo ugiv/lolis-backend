7145a1a29fc9d1c277ed6ac2bba6d9758da2f4b4
ugi valentino <ugiv46@ugis-MacBook-Air.local>
2024-05-26 13:00:59 +0700

    Change import process to access expres.js

--- index.mjs
+++ index.mjs
@@ -1,16 +1,15 @@
-const express = require("express");
-const app = express();
-const db = require("./queries/queries");
-import { Connector } from '@google-cloud/cloud-sql-connector';
+import express from "express";
 import pg from 'pg';
+import { Connector } from '@google-cloud/cloud-sql-connector';
+const { Pool } = pg;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { Pool } = pg;
+const db = require("./queries/queries");
 
 const connector = new Connector();
 const clientOpts = await connector.getOptions({
     instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
-    ipType: "PUBLIC"
+    ipType: 'PUBLIC'
 });
 
 const pool = new Pool({
@@ -21,6 +20,8 @@ const pool = new Pool({
     max: 5
 });
 const port = parseInt(process.env.PORT) || 8080;
+
+const app = express();
 
 app.use(cookieParser());
 app.use(cors({origin: "http://localhost:3000", credentials: true}));
